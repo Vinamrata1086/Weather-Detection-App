a28c0e3fdded7bf52c0d8835c0ad4d8c7518bb0f
Vinamrata1086 <59366156+Vinamrata1086@users.noreply.github.com>
2021-03-07 12:35:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -100,17 +100,11 @@ function displayResults(weather){
 }
 
 
-function dateBuilder (d) {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-  
-    return `${day}, ${date} ${month}, ${year}`;
-  }
+
+ function dateBuilder (d) {
+    var day = d.toDateString();
+    return `${day}`;
+}
 
   
 
